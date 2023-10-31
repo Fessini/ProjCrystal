5096f360e0e318c1ec9dca05b9f1c86c02cbffe3
Márcio Fessini <fessini@gmail.com>
2023-10-31 19:52:34 -0300

    Aula 05

--- ProjCrystal/frmPrincipal.Designer.vb
+++ ProjCrystal/frmPrincipal.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmPrincipal
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,32 @@ Partial Class frmPrincipal
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnAbrir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAbrir
+        '
+        Me.btnAbrir.Location = New System.Drawing.Point(131, 105)
+        Me.btnAbrir.Name = "btnAbrir"
+        Me.btnAbrir.Size = New System.Drawing.Size(226, 23)
+        Me.btnAbrir.TabIndex = 0
+        Me.btnAbrir.Text = "Abrir"
+        Me.btnAbrir.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnAbrir)
+        Me.Name = "frmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "frmPrincipal"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents btnAbrir As Button
 End Class
--- ProjCrystal/frmPrincipal.vb
+++ ProjCrystal/frmPrincipal.vb
@@ -1,3 +1,18 @@
-﻿Public Class frmPrincipal
+﻿Imports System.Data.SqlClient
 
+Public Class frmPrincipal
+    Private Sub btnAbrir_Click(sender As Object, e As EventArgs) Handles btnAbrir.Click
+        Dim conn As New SqlConnection("Server=CODE-DESENV;Database=Backup Mult;User Id=sa;Password=@mar147;")
+        Dim command As New SqlCommand("select nome from tb_cad_cliente", conn)
+        Dim da As New SqlDataAdapter(command)
+        Dim ds As New DataSet
+        da.Fill(ds)
+        ds.Tables(0).TableName = "Cliente"
+        '
+        Dim frm As New Form1
+        frm.TITULO_RELATORIO = "Primeiro Relatório"
+        frm.CAMINHO_RELATORIO = Application.StartupPath & "\Relatorios\rtpRelatorio1.rpt"
+        frm.DADOS_RELATORIO = ds
+        frm.Show()
+    End Sub
 End Class
